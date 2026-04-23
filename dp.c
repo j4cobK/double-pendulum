f114c6fa33b6104dd2218d65996a5ac6d3cf9fff
Jacob Kluetz <kluetzjacoba@johndeere.com>
2026-04-22 21:26:05 -0500

    fully functioning double pendulum

--- dp.c
+++ dp.c
@@ -8,7 +8,13 @@
 #define MASS_RADIUS 20
 #define LINE_THICKNESS 4
 #define L1 250
-#define L2 250
+#define L2 150
+#define G 1000
+#define TRACE_LENGTH 100
+
+Vector2 trace[TRACE_LENGTH];
+
+float l1, l2, m1, m2, phi1, phi2, phi1_d, phi2_d, phi1_dd, phi2_dd;
 
 Vector2 start_pos = (Vector2){WIDTH / 2, 0};
 
@@ -27,24 +33,78 @@ void draw_pendulum(float l, float phi, Vector2 start)
 
 void draw_double_pendulum(float l1, float l2, float phi1, float phi2, Vector2 start)
 {
-    draw_pendulum(l1, phi1, start);
 
     Vector2 end = get_end(start, l1, phi1);
+    // Draw bottom pendulum first so theres no overlap
     draw_pendulum(l2, phi2, end);
+
+    draw_pendulum(l1, phi1, start);
+
+    // draw trace of second pendulum
+    Vector2 end_l2 = get_end(end, l2, phi2);
+
+    for (int i = TRACE_LENGTH - 1; i > 0; i--)
+    {
+        trace[i] = trace[i - 1];
+    }
+    trace[0] = end_l2;
+
+    for (int i = 0; i < TRACE_LENGTH; i++)
+    {
+        DrawPixelV(trace[i], RED);
+    }
+}
+
+void step(float dt)
+{
+    phi1_dd =  -G*(2*m1+m2)*sinf(phi1) - m2*G*sinf(phi1-2*phi2) - 2*sinf(phi1-phi2)*m2*(phi2_d*phi2_d*l2 + phi1_d*phi1_d*l1*cosf(phi1-phi2));
+    phi1_dd /= l1*(2*m1+m2-m2*cosf(2*phi1-2*phi2));
+
+    phi2_dd = 2*sinf(phi1-phi2)*(phi1_d*phi1_d*l1*(m1+m2) + G*(m1+m2)*cosf(phi1) + phi2_d*phi2_d*l2*m2*cosf(phi1-phi2));
+    phi2_dd /= l2*(2*m1+m2-m2*cosf(2*phi1-2*phi2));
+
+    //angualar velo
+
+    phi1_d += phi1_dd * dt;
+
+    phi2_d += phi2_dd * dt;
+
+    //angle itself
+    phi1 += phi1_d * dt;
+    phi2 += phi2_d * dt;
+}
+
+void init_solver()
+{
+    phi1 = GetRandomValue(-180, 180) * DEG2RAD;
+    phi2 = GetRandomValue(-360, 360) * DEG2RAD;
+    phi1_d = 0;
+    phi2_d = 0;
+    l1 = L1;
+    l2 = L2;
+    m1 = 2;
+    m2 = 2;
 }
 
 int main(void)
 {
     InitWindow(WIDTH, HEIGHT, "Double Pendulum Simulation");
 
-    SetTargetFPS(60);
+    SetTargetFPS(120);
 
+
+    init_solver();
     Vector2 start_pos = (Vector2){WIDTH / 2, 0};
     while (!WindowShouldClose())
     {
+        step(GetFrameTime());
         BeginDrawing();
-
-        draw_double_pendulum(L1, L2, DEG2RAD * 50, DEG2RAD * -30, start_pos);
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            init_solver();    
+        }
+        ClearBackground(BLACK);
+        draw_double_pendulum(l1, l2, phi1, phi2, start_pos);
         EndDrawing();
     }
 
